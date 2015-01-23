a2162475a3831c3badbb316f63ef2a00ef46d0c3
Jose <Jose@iMac-de-Jose.local>
2015-01-23 21:30:02 +0100

    Implementacion clase para la API

--- bbbUp/bbbUp/DribbleApi.swift
+++ bbbUp/bbbUp/DribbleApi.swift
@@ -9,5 +9,25 @@
 import Foundation
 
 class DribbleApi {
-    <#properties and methods#>
+    let accessToken = "f6cfc7e8e2250bb0c41775f899ba6571da723d558d8263c9f71f485409b09abf"
+    
+    func loadShots(completion: ((AnyObject)->Void)!) {
+        
+        var urlString = "https://api.dribbble.com/v1/shots?access_token=" + accessToken
+        let session = NSURLSession.sharedSession()
+        let shotUrl = NSURL(string: urlString)
+        var task = session.dataTaskWithURL(shotUrl!){
+            (data, response)-> Void in
+            if error != nil {
+                println(error.localizedDescription)
+            }
+            else {
+            
+            }
+        
+        }
+    
+        task.resume()
+    
+    }
 }
