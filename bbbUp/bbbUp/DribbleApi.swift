a1e6658677236c502cd9811c662e3009537a0a13
Jose <Jose@iMac-de-Jose.local>
2015-01-23 20:27:30 +0100

    Primera subida

---
+++ bbbUp/bbbUp/DribbleApi.swift
@@ -0,0 +1,13 @@
+//
+//  DribbleApi.swift
+//  bbbUp
+//
+//  Created by Jose on 23/1/15.
+//  Copyright (c) 2015 joseadame. All rights reserved.
+//
+
+import Foundation
+
+class DribbleApi {
+    <#properties and methods#>
+}
---
+++ bbbUp/bbbUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bbbUp
+//
+//  Created by Jose on 23/1/15.
+//  Copyright (c) 2015 joseadame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
