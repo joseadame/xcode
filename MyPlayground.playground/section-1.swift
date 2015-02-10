23e0110461778fa512e45dbc5c4b4fc852fff807
Jose <Jose@iMac-de-Jose.local>
2015-02-10 16:11:46 +0100

    Proyecto sencillo xcode

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,19 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+str+="Pepe"
+
+var j = 2
+
+for var i = 0; i < 5 ; i++ {
+    j+=i
+}
+j
+
+println("Hello, world")
+//prints out hello world
+
+let numberOfPlayers = 4
